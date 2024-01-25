31f7b34e154f2fa3e13d4eda5acd24290b58ca17
IdoAviramMoveo <idoa@moveo.co.il>
2024-01-25 18:18:42 +0200

    Added the Header component - only for mobile

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import "./Header.scss";
+import hamburgerLogo from "../../assets/images/hamburgerLogo.svg";
+import epicureLogo from "../../assets/images/epicureLogo.png";
+import bagLogo from "../../assets/images/bagLogo.svg";
+import searchLogo from "../../assets/images/searchLogo.svg";
+import userLogo from "../../assets/images/userLogo.svg";
+
+const Header = () => {
+  return (
+    <>
+      <header className='header'>
+        <img className='hamburger-menu' src={hamburgerLogo} alt='Menu' />
+
+        <img className='app-logo' src={epicureLogo} alt='Epicure Logo' />
+
+        <div className='navbar-icons'>
+          <img className='icon' src={searchLogo} alt='Search' />
+          <img className='icon' src={userLogo} alt='User Account' />
+          <img className='icon' src={bagLogo} alt='Shopping Bag' />
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
