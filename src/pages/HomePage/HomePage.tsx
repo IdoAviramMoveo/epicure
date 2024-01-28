e55d85f2610017ce2f79a3fad57a1376fa5e9248
IdoAviramMoveo <idoa@moveo.co.il>
2024-01-28 15:16:23 +0200

    Added the Hero component - only for mobile

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 };
