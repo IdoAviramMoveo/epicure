69f0ad0bc349f66fb8a29b40ad6e0cd9961b6638
IdoAviramMoveo <idoa@moveo.co.il>
2024-01-30 15:02:17 +0200

    Fixed scss and changed constants names

--- src/constants/ChefOfTheWeekData.ts
+++ src/constants/ChefOfTheWeekData.ts
@@ -1,15 +1,15 @@
-import YossiRestaurantsData from "./YossiRestaurantsData";
+import ChefOfTheWeekRestaurantsData from "./ChefOfTheWeekRestaurantsData";
 import { ChefData } from "../models/types";
 
 import chefYossi from "../assets/images/chefYossi.png";
 
-const ChefYossiData: ChefData = {
+const ChefOfTheWeekData: ChefData = {
   title: "Chef Of The Week:",
   chefName: "Yossi Shitrit",
   image: chefYossi,
   chefDescription:
     "Chef Yossi Shitrit has been living and breathing his culinary dreams for more than two decades, including running the kitchen in his first restaurant, the fondly-remembered Violet, located in Moshav Udim. Shitrit's creativity and culinary acumen born of long experience are expressed in the every detail of each and every dish.",
-  restaurants: YossiRestaurantsData,
+  restaurants: ChefOfTheWeekRestaurantsData,
 };
 
-export default ChefYossiData;
+export default ChefOfTheWeekData;
