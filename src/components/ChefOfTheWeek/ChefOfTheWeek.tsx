4872263abee3dd7f3ea358d0583f861fb6e20a38
IdoAviramMoveo <idoa@moveo.co.il>
2024-01-30 15:02:17 +0200

    Fixed scss and changed constants names

--- src/components/ChefOfTheWeek/ChefOfTheWeek.tsx
+++ src/components/ChefOfTheWeek/ChefOfTheWeek.tsx
@@ -1,24 +1,26 @@
 import "./ChefOfTheWeek.scss";
 
 import CardsGallery from "../../components/CardsGallery/CardsGallery";
-import ChefYossiData from "../../constants/ChefYossiData";
+import ChefOfTheWeekData from "../../constants/ChefOfTheWeekData";
 import { CardType } from "../../models/types";
 
 const ChefOfTheWeek = () => {
+  const backgroundImageStyle = {
+    backgroundImage: `url(${ChefOfTheWeekData.image})`,
+  };
   return (
     <>
       <div className='chef-container'>
-        <h3 className='chef-title'>{ChefYossiData.title}</h3>
+        <h3 className='chef-title'>{ChefOfTheWeekData.title}</h3>
         <div className='image-description-container'>
-          <div className='chef-image-container '>
-            <img src={ChefYossiData.image} alt='Chef Yossi' className='chef-image' />
-            <h3 className='chef-name'>{ChefYossiData.chefName}</h3>
+          <div className='chef-image-container' style={backgroundImageStyle}>
+            <h3 className='chef-name'>{ChefOfTheWeekData.chefName}</h3>
           </div>
-          <p className='chef-description'>{ChefYossiData.chefDescription}</p>
+          <p className='chef-description'>{ChefOfTheWeekData.chefDescription}</p>
         </div>
       </div>
-      <div className='yossi-restaurants'>
-        <CardsGallery cardsData={ChefYossiData.restaurants} cardType={CardType.YossiRestaurantType} />
+      <div className='chef-restaurants'>
+        <CardsGallery cardsData={ChefOfTheWeekData.restaurants} cardType={CardType.YossiRestaurantType} />
       </div>
     </>
   );
