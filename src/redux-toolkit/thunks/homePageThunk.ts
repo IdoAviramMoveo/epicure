61b860ca896e1b2efed38beb5d659c55b1ad360e
IdoAviramMoveo <idoa@moveo.co.il>
2024-02-08 10:12:40 +0200

    Added type

--- src/redux-toolkit/thunks/homePageThunk.ts
+++ src/redux-toolkit/thunks/homePageThunk.ts
@@ -13,30 +13,33 @@ interface HomePageData {
   chefOfTheWeek: ChefData;
 }
 
-export const fetchHomePageData = createAsyncThunk<HomePageData>("homePage/fetchData", async () => {
-  const [restaurantsResponse, dishesResponse, chefsResponse] = await Promise.all([
-    axios.get("http://localhost:3000/restaurants"),
-    axios.get("http://localhost:3000/dishes"),
-    axios.get("http://localhost:3000/chefs"),
-  ]);
+export const fetchHomePageData = createAsyncThunk<HomePageData>(
+  "homePage/fetchData",
+  async (): Promise<HomePageData> => {
+    const [restaurantsResponse, dishesResponse, chefsResponse] = await Promise.all([
+      axios.get("http://localhost:3000/restaurants"),
+      axios.get("http://localhost:3000/dishes"),
+      axios.get("http://localhost:3000/chefs"),
+    ]);
 
-  const restaurants = transformRestaurantData(restaurantsResponse.data);
-  const dishes = transformDishData(dishesResponse.data);
+    const restaurants = transformRestaurantData(restaurantsResponse.data);
+    const dishes = transformDishData(dishesResponse.data);
 
-  const chefsOfTheWeekPromises = chefsResponse.data
-    .filter((chef: BackendChef) => chef.canBeChefOfTheWeek)
-    .map(async (chef: BackendChef) => {
-      const chefRestaurantsResponse = await axios.get(`http://localhost:3000/chefs/${chef._id}/with-restaurants`);
-      return transformChefData(chef, transformRestaurantData(chefRestaurantsResponse.data.restaurants));
-    });
+    const chefsOfTheWeekPromises = chefsResponse.data
+      .filter((chef: BackendChef) => chef.canBeChefOfTheWeek)
+      .map(async (chef: BackendChef) => {
+        const chefRestaurantsResponse = await axios.get(`http://localhost:3000/chefs/${chef._id}/with-restaurants`);
+        return transformChefData(chef, transformRestaurantData(chefRestaurantsResponse.data.restaurants));
+      });
 
-  const chefsOfTheWeek = await Promise.all(chefsOfTheWeekPromises);
+    const chefsOfTheWeek: ChefData[] = await Promise.all(chefsOfTheWeekPromises);
 
-  const chefOfTheWeek: ChefData = chefsOfTheWeek[Math.floor(Math.random() * chefsOfTheWeek.length)];
+    const chefOfTheWeek: ChefData = chefsOfTheWeek[Math.floor(Math.random() * chefsOfTheWeek.length)];
 
-  return {
-    restaurants,
-    dishes,
-    chefOfTheWeek,
-  };
-});
+    return {
+      restaurants,
+      dishes,
+      chefOfTheWeek,
+    };
+  }
+);
