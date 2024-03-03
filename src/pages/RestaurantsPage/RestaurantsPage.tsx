c2d12133c2dedaba2c369ca0d3d19ef88fc15a14
IdoAviramMoveo <idoa@moveo.co.il>
2024-03-03 15:57:57 +0200

    Added filters

--- src/pages/RestaurantsPage/RestaurantsPage.tsx
+++ src/pages/RestaurantsPage/RestaurantsPage.tsx
@@ -1,32 +1,58 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Fade } from "react-awesome-reveal";
+import { Fade, Slide } from "react-awesome-reveal";
 
 import "./RestaurantsPage.scss";
 import { RootState, AppDispatch } from "../../redux-toolkit/store";
 import { fetchRestaurantsPageData } from "../../redux-toolkit/thunks/restaurantsPageThunk";
+import { fetchHomePageData } from "../../redux-toolkit/thunks/homePageThunk";
 import Card from "../../components/Card/Card";
 import { CardProps } from "../../models/types";
 
 const RestaurantsPage = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const [activeFilter, setActiveFilter] = useState("All");
 
   const { restaurants } = useSelector((state: RootState) => state.restaurantsPage);
+  const { popularRestaurants } = useSelector((state: RootState) => state.homePage);
 
   useEffect(() => {
     dispatch(fetchRestaurantsPageData());
-  }, [dispatch]);
+    if (!popularRestaurants.cards.length) {
+      dispatch(fetchHomePageData());
+    }
+  }, [dispatch, popularRestaurants, restaurants, popularRestaurants.cards]);
+
+  const handleFilterClick = (filter: string) => {
+    setActiveFilter(filter);
+  };
+
+  const filteredCards = () => {
+    if (activeFilter === "New") {
+      return restaurants.cards.slice(-9).map((card: CardProps) => <Card {...card} className='restaurants-page-card' key={card.title} />);
+    } else if (activeFilter === "Most Popular") {
+      return popularRestaurants.cards.map((card: CardProps) => <Card {...card} className='restaurants-page-card' key={card.title} />);
+    }
+    return restaurants.cards.map((card: CardProps) => <Card {...card} className='restaurants-page-card' key={card.title} />);
+  };
 
   return (
     <>
       <div className='restaurants-page-container'>
         <h2 className='title'>{restaurants.title}</h2>
+        <div className='restaurants-filter-buttons'>
+          {["All", "New", "Most Popular"].map((filter) => (
+            <button
+              key={filter}
+              className={`filter-button ${activeFilter === filter ? "active" : ""}`}
+              onClick={() => handleFilterClick(filter)}
+            >
+              {filter}
+            </button>
+          ))}
+        </div>
         <div className='restaurants-container'>
-          <Fade>
-            {restaurants.cards.map((card: CardProps) => (
-              <Card {...card} className='restaurants-page-card' key={card.title} />
-            ))}
-          </Fade>
+          <Fade>{filteredCards()}</Fade>
         </div>
       </div>
     </>
